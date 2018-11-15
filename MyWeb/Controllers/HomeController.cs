2d0dde5793f368c25629720f2c9357cef6ac73d3
yirer <eolo4@hotmail.com>
2018-11-15 18:01:04 +0000

    Hello

--- MyWeb/Controllers/HomeController.cs
+++ MyWeb/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello World.";
 
             return View();
         }
